e488de226a58fb39a74eae1e1578a687b57a2c8e
moon.pan <px5215201314@163.com>
2026-02-06 18:47:53 +0800

    home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,65 +1,68 @@
-import Image from "next/image";
+"use client";
 
-export default function Home() {
+import React, { useState, useEffect } from "react";
+import { Input } from "antd";
+import { CalendarOutlined, ScheduleOutlined, SearchOutlined } from "@ant-design/icons";
+import '@/styles/home.css'
+
+const Home = () => {
+  const [searchkey, setSearchkey] = useState('')
+
+  const search = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    let value = e.currentTarget.value.trim()
+    if (value) {
+      setSearchkey('')
+      console.log(value)
+      // window.open(`/search?searchkey=${value}`, '_blank');
+    }
+  }
+
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+    <React.Fragment>
+      <div className="header">
+        <a className="logo" href="/">
+          <img src="/favicon.svg" alt="appLogo" />
+          <div className="text">Life Timer</div>
+        </a>
+      </div>
+      <div className="main">
+        <div>
+          <Input placeholder="搜索" value={searchkey} onChange={(e) => setSearchkey(e.target.value)} prefix={<SearchOutlined />} onPressEnter={search} />
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+        <div className="status">
+          <div className="status-card warning">
+            <div>即将过期</div>
+            <div className="text-[#d89614]">1</div>
+          </div>
+          <div className="status-card danger">
+            <div>已过期</div>
+            <div className="text-[#dc4446]">3</div>
+          </div>
         </div>
-      </main>
-    </div>
-  );
-}
+        <div className="grid-ul">
+          <div className="card-li">
+            <h2>牛奶</h2>
+            <span>已过期</span>
+            <div>生鲜</div>
+            {/* <div>冰箱</div> */}
+          </div>
+          <div className="card-li">
+            <h2>牛奶</h2>
+            <span>已过期</span>
+            <div>生鲜</div>
+            {/* <div>冰箱</div> */}
+          </div>
+          <div className="card-li">
+            <h2>牛奶</h2>
+            <span>已过期</span>
+            <div>生鲜</div>
+            {/* <div>冰箱</div> */}
+          </div>
+        </div>
+      </div>
+      <div className="footer"></div>
+    </React.Fragment>
+  )
+};
+
+export default Home;
